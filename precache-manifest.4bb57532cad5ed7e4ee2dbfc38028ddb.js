c4ff93461be563005d8154fccd3f3351607b4a02
Kaymind <lempelziv@gmail.com>
2019-10-30 16:43:50 +0700

    Updates

---
+++ precache-manifest.4bb57532cad5ed7e4ee2dbfc38028ddb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d69d04f1a6a5738247d7d67d677d76c",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "da49a4837aabc9ed9c46",
+    "url": "/todo-app/static/css/main.f34a33b5.chunk.css"
+  },
+  {
+    "revision": "4872c19ba5b6af7ae643",
+    "url": "/todo-app/static/js/2.f99a46cf.chunk.js"
+  },
+  {
+    "revision": "da49a4837aabc9ed9c46",
+    "url": "/todo-app/static/js/main.0e11104f.chunk.js"
+  },
+  {
+    "revision": "f5457ae2305694b347f4",
+    "url": "/todo-app/static/js/runtime-main.ecc201f8.js"
+  }
+]);
